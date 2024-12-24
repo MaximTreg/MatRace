e113556d4cf6e08d3f3da2dd0153ec4e529e7749
Maxim <326632maximtr@gmail.com>
2024-12-24 21:40:01 +0700

    Добавлена таблица UserGroup

--- app/models.py
+++ app/models.py
@@ -9,11 +9,7 @@ from app import login
 def load_user(id):
     return User.query.get(int(id))
 
-user_groups = db.Table('user_groups',
-    db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('group_id', db.Integer, db.ForeignKey('groups.id'), primary_key=True)
-)
+
 
 
 class UserGroup(db.Model):
